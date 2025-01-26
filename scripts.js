701fc768eb5c30c01ab5f22c322db8acc53b01f9
NeimOli <160206207+NeimOli@users.noreply.github.com>
2025-01-26 12:22:55 +0545

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('navbar-logo').classList.add('animate-fadeIn');
+    document.getElementById('logo').classList.add('animate-scaleUp');
+    document.getElementById('navbar-title').classList.add('animate-slideIn');
+  };
+  
